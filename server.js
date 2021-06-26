3cd62a9fb682e9d76d56184170a6c75ae2060e3e
Thilak <tshankarappa@esri.com>
2021-06-26 15:37:30 -0700

    added s3

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 "use strict";
  
 var app = require("./app.js");
- 
+let store = require('greenlock-storage-s3').create({
+    accessKeyId: 'AKIA3S3LRSI357F72MO6'                // Replace with your accessKeyId
+    , secretAccessKey: 'S/dRHwibdmo40VwBhaNM85swZXti0prPm8uNuLuw'      // Replace with your secretAccessKey
+    , bucketRegion: 'us-west-2'            // Replace with your bucketRegion
+    , bucketName: 'cert-bot'                // Replace with your bucketName
+    , configDir: 'acme/'                    // Recommended
+    , accountsDir: 'accounts/'              // Recommended
+    , debug: true                           // Debug
+});
+
+
 require("greenlock-express")
     .init({
         packageRoot: __dirname,
-        configDir: "./greenlock.d",
- 
+        //configDir: './greenlock.d/',
+        store: store,
         // contact for security and critical bug notices
         maintainerEmail: "tshankarappa@esri.com",
  
